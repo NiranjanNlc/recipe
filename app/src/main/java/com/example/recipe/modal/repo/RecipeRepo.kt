0c070fc188d037fd953c5981e19fa7543c537368
niranjannlc <niranjannlc10@gmail.com>
2021-09-21 19:29:37 +0800

    implemenation alyout of repo class for reterofit call

--- app/src/main/java/com/example/recipe/modal/repo/RecipeRepo.kt
+++ app/src/main/java/com/example/recipe/modal/repo/RecipeRepo.kt
@@ -1,4 +1,55 @@
 package com.example.recipe.modal.repo
 
-class RecipeRepo {
+import androidx.lifecycle.MutableLiveData
+import com.example.recipe.modal.data.Recipe
+import com.example.recipe.modal.dataItem.MealList
+import com.example.recipe.modal.dataItem.MealX
+import com.example.recipe.modal.dataItem.Reciepe
+import com.example.recipe.modal.reterofit.RetrofitService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class RecipeRepo(val  service:RetrofitService)
+{
+        var reciepe= MutableLiveData<List<MealX>>()
+        val recipeList  = MutableLiveData<List<Recipe>>()
+
+        fun getReciepeList()
+        {
+            val response = service.getReciepeList()
+            response.enqueue(object : Callback<MealList>
+            {
+
+                override fun onResponse(call: Call<MealList>?, response: Response<MealList>?)
+                {
+                    println(" ")
+
+                }
+
+                override fun onFailure(call: Call<MealList>?, t: Throwable?)
+                {
+                     println(" ")
+                }
+
+            })
+
+        }
+         fun getReciepe( id:String)
+         {
+             val response = service.getReciepeParticlular(id)
+             response.enqueue(object : Callback<Reciepe>
+             {
+                 override fun onResponse(call: Call<Reciepe>?, response: Response<Reciepe>?) {
+
+                 }
+
+                 override fun onFailure(call: Call<Reciepe>?, t: Throwable?) {
+
+                 }
+
+             })
+
+         }
+
 }
