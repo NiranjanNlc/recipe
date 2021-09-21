972284410de760d0749eabc7cedfb3a1a6635fb7
niranjannlc <niranjannlc10@gmail.com>
2021-09-21 19:44:49 +0800

    connected viewmodal eith reo

--- app/src/main/java/com/example/recipe/viewmodal/ReciepeViewModal.kt
+++ app/src/main/java/com/example/recipe/viewmodal/ReciepeViewModal.kt
@@ -3,5 +3,17 @@ package com.example.recipe.viewmodal
 import androidx.lifecycle.ViewModel
 import com.example.recipe.modal.repo.RecipeRepo
 
-class ReciepeViewModal(repository: RecipeRepo) : ViewModel()
-{  }
+class ReciepeViewModal(val repository: RecipeRepo) : ViewModel()
+{
+    val reciepList = repository.recipeList
+    val reciepe = repository.reciepe
+
+    fun getReciepeList()
+    {
+      repository.getReciepeList()
+    }
+    fun getReciepeParticular(id:String)
+    {
+        repository.getReciepe(id)
+    }
+}
