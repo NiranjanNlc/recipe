1b7c2ddebecaafe1576874715340ca914899189d
niranjannlc <niranjannlc10@gmail.com>
2021-09-22 15:53:45 +0800

    not wise to test main class in android studio

--- app/src/main/java/com/example/recipe/view/MainActivity.kt
+++ app/src/main/java/com/example/recipe/view/MainActivity.kt
@@ -3,10 +3,18 @@ package com.example.recipe.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.recipe.R
+import com.example.recipe.modal.repo.RecipeRepo
+import com.example.recipe.modal.reterofit.RetrofitService
+import com.example.recipe.viewmodal.ReciepeViewModal
+import com.example.recipe.viewmodal.ViewModalFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val repository = RecipeRepo(RetrofitService.getInstance())
+        val sampleViewModal= ViewModalFactory( repository).create(ReciepeViewModal::class.java)
+        sampleViewModal.getReciepeList()
+        println(" this data "+ sampleViewModal.reciepList.toString())
     }
 }
