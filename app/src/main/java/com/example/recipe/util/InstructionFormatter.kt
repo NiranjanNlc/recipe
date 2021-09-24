ea8172458d0d534d6b165569dae578b800cc3909
niranjannlc <niranjannlc10@gmail.com>
2021-09-24 12:13:19 +0800

    using reflection api

---
+++ app/src/main/java/com/example/recipe/util/InstructionFormatter.kt
@@ -0,0 +1,15 @@
+package com.example.recipe.util
+
+import com.example.recipe.modal.dataItem.Meal
+
+object InstructionFormatter
+{
+    fun formatInstruction(meal : Meal)
+    {
+        for (prop in meal::class.members)
+        {
+            println("${prop.name} = ${meal}")
+        }
+    }
+
+    }
--- app/src/main/java/com/example/recipe/view/ReciepeFragment.kt
+++ app/src/main/java/com/example/recipe/view/ReciepeFragment.kt
@@ -6,10 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.recipe.R
+import com.example.recipe.modal.repo.RecipeRepo
+import com.example.recipe.modal.reterofit.RetrofitService
+import com.example.recipe.util.InstructionFormatter
+import com.example.recipe.viewmodal.ReciepeViewModal
+import com.example.recipe.viewmodal.ViewModalFactory
 
 
 class ReciepeFragment : Fragment()
 {
+    private lateinit var sampleViewModal:ReciepeViewModal
+    private lateinit var binding:
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -20,8 +27,31 @@ class ReciepeFragment : Fragment()
         savedInstanceState: Bundle?
     ): View?
     {
-
+           sampleViewModal= initialiseViewModal()
+          observeChange()
         return inflater.inflate(R.layout.full_recipe, container, false)
     }
+    private fun initialiseViewModal(): ReciepeViewModal {
+        val repository = RecipeRepo(RetrofitService.getInstance())
+        val sampleViewModal = ViewModalFactory(repository).create(ReciepeViewModal::class.java)
+        return sampleViewModal
+    }
+    private fun observeChange() {
 
+        sampleViewModal.getReciepeParticular("52807")
+        sampleViewModal.reciepe.observe(this, {
+            println(" this data " + sampleViewModal.reciepe.value.toString())
+            val meal  = sampleViewModal.reciepe.value
+            if (meal != null) {
+                setTheUi()
+                InstructionFormatter.formatInstruction(meal.meals[0])
+            }
+        }
+        )
+    }
+
+    private fun setTheUi() {
+
+
+    }
 }
